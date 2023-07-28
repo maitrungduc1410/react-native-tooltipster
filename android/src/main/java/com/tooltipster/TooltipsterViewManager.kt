67ac17c522c0a4f1b0dd759c251e434fe51769bb
Mai Trung Duc <maitrungduc1410@gmail.com>
2023-07-28 23:02:08 +0800

    chore: initial commit

---
+++ android/src/main/java/com/tooltipster/TooltipsterViewManager.kt
@@ -0,0 +1,20 @@
+package com.tooltipster
+
+import android.graphics.Color
+import android.view.View
+import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.annotations.ReactProp
+
+class TooltipsterViewManager : SimpleViewManager<View>() {
+  override fun getName() = "TooltipsterView"
+
+  override fun createViewInstance(reactContext: ThemedReactContext): View {
+    return View(reactContext)
+  }
+
+  @ReactProp(name = "color")
+  fun setColor(view: View, color: String) {
+    view.setBackgroundColor(Color.parseColor(color))
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  TooltipsterExample
+//
+
+import Foundation
